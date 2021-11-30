4e4717ce5affd928bc2caaad80969b2754a5e14d
Frederik Brauer Nissen <fred145a@edu.zealand.dk>
2021-11-30 12:25:03 +0100

    Version 1.0.1

---
+++ Brugere/Brugere.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GamingSiteProject.Brugere
+{
+    public class BrugereModel : PageModel
+    {
+        public void OnGet()
+        {
+            //Hej Mads
+        }
+    }
+}
