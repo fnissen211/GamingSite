85457018ec8c990347f2945d53cd5acc99e75c97
Frederik Brauer Nissen <fred145a@edu.zealand.dk>
2021-12-06 12:20:20 +0100

    version 2.0.2

---
+++ Pages/UserList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamingSiteProject.Pages
+{
+    public class UserList
+    {
+        private string _file = @"data\Users.json";
+
+        
+
+        public UserList()
+        {
+            
+        }
+    }
+}
